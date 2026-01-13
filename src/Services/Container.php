04d2d7a4d9baedb3b57268cf4b3b61e28639c7d9
Yjaatouri <yahyajaatouri2000@gmail.com>
2026-01-13 11:39:06 +0100

    add Container.php

---
+++ src/Services/Container.php
@@ -0,0 +1,16 @@
+<?php
+
+class Container {
+    private $services = [];
+
+    public function set($name, $service) {
+        $this->services[$name] = $service;
+    }
+
+    public function get($name) {
+        if (!isset($this->services[$name])) {
+            throw new Exception("Service $name not found");
+        }
+        return $this->services[$name];
+    }
+}
