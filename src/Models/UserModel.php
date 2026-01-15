a20801a945d09b70c0d459897cbb9bcdf79991a4
Yjaatouri <yahyajaatouri2000@gmail.com>
2026-01-15 10:28:28 +0100

    start creating models (1est user model)

---
+++ src/Models/UserModel.php
@@ -0,0 +1,38 @@
+<?php
+namespace Models;
+use PDO;
+
+class UserModel{
+    private $db;
+    
+    public function __construct(PDO $db){
+        $this->db = $db;
+    } 
+    
+    public function register($email , $password , $name){
+        $hash = password_hash($password , PASSWORD_DEFAULT);
+        $stmt = this->db->prepare("INSERT INTO users(email , password_hash , name ) VALUES (?,?,?)");
+        return $stmt->execute([$email,$hash , $name]);
+    }
+
+    public function login($email , $password){
+        $stmt = this->db->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->execute([$email]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($user && password_verify($password , $user['password_hash'])) {
+            return $user;
+        }
+        return false;
+    }
+    public function gatUserById($id){
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE id = ?");
+        $stmt ->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    public function updatePoints($userId , $newTotla , $newEarned = null){
+        $sql .= "WHERE id = ?";
+        $params[] = $userId;
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute($params);
+    }
+}
